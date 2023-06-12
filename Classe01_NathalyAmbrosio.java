5426d486cb61f7057b33386e569ce8424ccd70cd
NathalyAmbrosio <131813980+NathalyAmbrosio@users.noreply.github.com>
2023-06-12 16:49:29 -0300

    Update Classe01_NathalyAmbrosio.java
    asd

--- Classe01_NathalyAmbrosio.java
+++ Classe01_NathalyAmbrosio.java
@@ -14,100 +14,20 @@ public class Alimento {
     private String dataValidade;
     private String descricao;
 
-    // Construtor
-    public Alimento(String nome, String categoria, double preco, int quantidade, String marca, boolean organico,
+    public Alimento(String nome, int calorias, double preco, int quantidade, String marca, boolean contemLactose,
                     boolean vegano, boolean semGluten, String dataValidade, String descricao) {
         this.nome = nome;
         this.calorias = calorias;
         this.preco = preco;
         this.quantidade = quantidade;
         this.marca = marca;
-        this.organico = contemLactose;
-        this.vegano = vegano;
-        this.semGluten = semGluten;
-        this.dataValidade = dataValidade;
-        this.descricao = descricao;
-    }
-
-    // Getters e Setters
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-   public int getCalorias() {
-        return calorias;
-    }
-
-    public void setCalorias(int calorias) {
-        this.calorias = calorias;
-    }
-
-
-    public double getPreco() {
-        return preco;
-    }
-
-    public void setPreco(double preco) {
-        this.preco = preco;
-    }
-
-    public int getQuantidade() {
-        return quantidade;
-    }
-
-    public void setQuantidade(int quantidade) {
-        this.quantidade = quantidade;
-    }
-
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public boolean iscontemLactose() {
-        return contemLactose;
-    }
-
-    public void setContemLactose(boolean contemLactose) {
         this.contemLactose = contemLactose;
-    }
-
-    public boolean isVegano() {
-        return vegano;
-    }
-
-    public void setVegano(boolean vegano) {
         this.vegano = vegano;
-    }
-
-    public boolean isSemGluten() {
-        return semGluten;
-    }
-
-    public void setSemGluten(boolean semGluten) {
         this.semGluten = semGluten;
-    }
-
-    public String getDataValidade() {
-        return dataValidade;
-    }
-
-    public void setDataValidade(String dataValidade) {
         this.dataValidade = dataValidade;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
         this.descricao = descricao;
     }
+
+    // Getters e Setters omitidos por brevidade
 }
+
